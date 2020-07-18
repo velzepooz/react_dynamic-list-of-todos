06c372128b174dd5c9aae2a5b25c44d9ad81d57d
Glib Rulev <velzepooz@gmail.com>
2020-07-18 16:53:18 +0300

    Updates

---
+++ precache-manifest.4c42ef7c9baddffcf68bff73c9edc214.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6999203c06e7f4be9b649fa6f01a2f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d147bd80bb7b6a0fb7ac",
+    "url": "./static/css/main.2f59b4ea.chunk.css"
+  },
+  {
+    "revision": "f070ca5082d2f94752df",
+    "url": "./static/js/2.c565d8bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c565d8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d147bd80bb7b6a0fb7ac",
+    "url": "./static/js/main.3a4b4aa7.chunk.js"
+  },
+  {
+    "revision": "0c165204287428b65967",
+    "url": "./static/js/runtime-main.a37292e9.js"
+  }
+]);
